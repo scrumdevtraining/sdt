a89365c5d09b1d0343da97313c9ee3904ff02295
yoyo <yoyo@mail.com>
2016-08-25 16:20:37 +0800

    PowerOf2

---
+++ src/main/java/yose/Decomposition.java
@@ -0,0 +1,26 @@
+package yose;
+
+import java.util.List;
+
+
+public class Decomposition {
+	
+	private int number;
+	private List decomposition;
+	
+	public Decomposition(int number, List<Integer> arr){
+		this.number = number;
+		this.decomposition = arr;
+	}
+
+//	public boolean isAlive() {
+//		return alive;
+//	}
+//
+//	public void setAlive(boolean alive) {
+//		this.alive = alive;
+//	}
+//	
+	
+
+}
