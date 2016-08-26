f25371d2c02ff18fea9f35ecf91162450c4e5a34
yoyo <yoyo@mail.com>
2016-08-26 14:59:18 +0800

    string guard

---
+++ src/main/java/yose/DecompositionError.java
@@ -0,0 +1,16 @@
+package yose;
+
+import java.util.List;
+
+
+public class DecompositionError {
+	
+	private String number;
+	private String error;
+	
+	public DecompositionError(String number, String error){
+		this.number = number;
+		this.error = error;
+	}
+	
+}
